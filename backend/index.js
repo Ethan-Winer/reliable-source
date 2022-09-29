bbb0a35f30de6bcf266988a827334754cb63312e
Ethan-Winer <ethan.m.winer@gmail.com>
2022-09-29 14:59:05 -0400

    getting random set of rows works

--- backend/index.js
+++ backend/index.js
@@ -4,36 +4,56 @@ const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database(':memory:');
 
+var factList = [];
+
+
 db.run('CREATE TABLE facts (id INTEGER PRIMARY KEY AUTOINCREMENT, fact TEXT)');
 
+db.serialize();
 
-//get number of rows in a table
-// console.log(db.get("SELECT COUNT(*) as 'count' FROM facts", [], (err, row) => {
-//   console.log(row.count);
-// }));
+// for (let i = 0; i < 100; i++) {
+//   db.run("INSERT INTO facts (fact) VALUES ('test" + i + "')");
+
+// }
+
 
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/data', (req, res) => {
-  db.all('SELECT * FROM facts', [], (err, rows) => {
-    console.log(rows);
-    res.send(rows);
+  db.get("SELECT COUNT(*) as 'count' FROM facts", [], (err, row) => {
+    let rand = Math.floor(Math.random() * (row.count - 20));
+    console.log(rand);
+    db.all("SELECT * FROM facts LIMIT " + rand + ", 20", [], (err, rows) => {
+      res.send(rows);
+
+    });
   });
 });
 
+
 app.post('/', (req, res) => {
   if (req.body.fact != undefined) {
-    // db.run('INSERT INTO facts (fact) VALUES (' + req.body.fact + ')')
     db.run("INSERT INTO facts (fact) VALUES ('" + req.body.fact + "')");
     res.sendStatus(200);
   }
   else {
     res.sendStatus(400);
   }
-
-
 });
 
 app.listen(8080);
+
+
+// setInterval(() => {
+//   db.get("SELECT COUNT(*) as 'count' FROM facts", [], (err, row) => {
+//     let rand = Math.floor(Math.random() * (row.count - 20));
+//     console.log(rand);
+//     db.all("SELECT * FROM facts LIMIT " + rand + ", 20", [], (err, rows) => {
+//       console.log(rows);
+//     });
+
+//   });
+
+// }, 1000);
