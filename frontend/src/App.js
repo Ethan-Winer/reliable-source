cd889627ef05fb885e5e3d26ca713b2187f2f0c9
Ethan-Winer <ethan.m.winer@gmail.com>
2022-11-26 18:06:00 -0500

    moving stuff to desktop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
-import Splashscreen from './components/splashscreen/Splashscreen'
+import Splashscreen from './components/splashscreen/Splashscreen';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Splashscreen></Splashscreen>
-    </div >
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showSplashscreen: true
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        showSplashscreen: false
+      });
+    }, 9000);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.showSplashscreen && <Splashscreen></Splashscreen>}
+        <h1>done</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
